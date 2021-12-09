d7adddaa218529508c3735c0deeb47a636f85d85
Dulce Fabiola <dulcefabiola.rm@gmail.com>
2021-12-09 13:09:37 -0600

    paises terminado

--- src/Router.js
+++ src/Router.js
@@ -1,9 +1,12 @@
 //1.IMPORTACIONES
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Countries from "./components/Countries";
+import Country from "./components/Country";
 import CRUD from "./components/CRUD";
 import Home from "./components/Home";
 import Layout from "./components/Layout";
+import data from "./countries.json";
 
 //2.FUNCION
 const Router = () => {
@@ -19,7 +22,10 @@ const Router = () => {
             {/* /crud o crud sin slach, funciona sin tema
           path es el camino a seguir
            */}
-            <Route path="/crud" element={<CRUD />} />
+            <Route path="crud" element={<CRUD />} />
+            <Route path="countries" element={<Countries allCountries={data} />}>
+              <Route path=":id" element={<Country allCountries={data} />} />
+            </Route>
           </Route>
         </Routes>
       </BrowserRouter>
